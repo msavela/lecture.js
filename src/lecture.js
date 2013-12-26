e72d5391eb522d26c005480c0d09af8641af9dab
Matias Savela <msavela@gmail.com>
2013-12-26 23:19:01 +0200

    Initial commit.

---
+++ src/lecture.js
@@ -0,0 +1,80 @@
+(function(module, window) {
+	window[module] = {
+
+		listeners: {},
+
+		init: function(element) {
+			this.parent = element || document.querySelector('article');
+			this.deck = this.parent.querySelectorAll('section');
+
+			this.dispatch('init') && this.activate(0);
+
+			return this;
+		},
+		activate: function(n) {
+			if(this.index == n || n < 0 || n > this.deck.length - 1)
+				return;
+
+			if(this.index >= 0 && !this.dispatch('deactivate', {
+				index: this.index,
+				element: this.deck[this.index]
+			})) return;
+
+			for(var i=0;i<this.deck.length;i++) {
+				this.deck[i].className =
+					this.deck[i].className.replace(
+						RegExp('((active|inactive)|(before|after)(-\\d+)?)', 'g')
+					, '').trim();
+
+				if(i == n)
+					this.deck[i].classList.add('active');
+				else {
+					this.deck[i].classList.add('inactive', (i < n) ? 'before' : 'after',
+						((i < n) ? 'before' : 'after') + '-' + Math.abs(i - n));
+				}
+			}
+			this.index = n;
+			this.dispatch('activate', {
+				index: this.index,
+				element: this.deck[this.index]
+			});
+		},
+		next: function() {
+			this.dispatch('next', {
+				index: this.index
+			}) && this.activate(this.index + 1);
+		},
+		prev: function() {
+			this.dispatch('prev', {
+				index: this.index
+			}) && this.activate(this.index - 1);
+		},
+		on: function(event, callback) {
+			(this.listeners[event] || (this.listeners[event] = [])).push(callback);
+		},
+		dispatch: function(event, data) {
+			var ret = true;
+			if(!this.listeners[event]) return ret;
+			for(var i=0;i<this.listeners[event].length;i++)
+				if(this.listeners[event][i](data) == false)
+					ret = false;
+			return ret;
+		},
+	};
+
+	document.addEventListener('keydown', function(e) {
+		[
+			34, // Page down
+			32, // Space
+			13, // Enter
+			39, // Right
+			40  // Down
+		].indexOf(e.which) > -1 && window[module].next();
+		
+		[
+			33, // Page up
+			37, // Left
+			38  // Up
+		].indexOf(e.which) > -1 && window[module].prev();
+	});
+}('lecture', this));
